acabb4bd265edfdd28337ffa3c580c20d237ff5d
Thomas Mao <tm1813@gmail.com>
2021-08-24 06:55:36 +0800

    hello world from GET

--- src/clj/learn_deps/core.clj
+++ src/clj/learn_deps/core.clj
@@ -1,9 +1,19 @@
-(ns learn_deps.core)
+(ns learn_deps.core
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :as route]
+            [org.httpkit.server :refer [run-server]]
+            [cheshire.core :as json]))
+
+(defroutes app
+  (GET "/" [] "Hello world"))
 
 (defn -main []
-      (println "Goodbye World!"))
+  ;    (println "Goodbye World!")
+  (run-server app {:port 4000})
+  )
 
 (comment
   (-main
    ;; => #function[learn-deps.core/-main]
-))
+)
+  )
